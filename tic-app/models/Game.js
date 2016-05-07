74143a56586ea1b3480341d653d68bd92d7577e0
mapineda <mapineda1222@gmail.com>
2016-05-06 21:42:29 -0500

    work on game.js

--- tic-app/models/Game.js
+++ tic-app/models/Game.js
@@ -16,6 +16,7 @@ GameSchema.statics.playerMoves = function(gameId, rowIndex, colIndex, callback) 
 		callback({status: 400, message: 'gameId is required'});
 		return;
 	}
+
 	Game.findById(gameId, function (err, game) {
 		if (err || !game) {
 	  		callback({status: 403, message: 'Invalid gameId was provided'});
@@ -25,9 +26,37 @@ GameSchema.statics.playerMoves = function(gameId, rowIndex, colIndex, callback) 
 		if (!game.board) {
 			game.board = [ [null, null, null], [null, null, null], [null, null, null]];
 		}
-  });
 
+    var symbol = playerSymbol(game.playerTurn);
+		game.board[rowIndex][colIndex] = symbol;
+		game.playerTurn = 1 - game.playerTurn;
+		var winnerSymbol = checkForWinner(game);
+		if (winnerSymbol)
+		{
+			var winnerNumber = playerNumber(winnerSymbol);
+			if (winnerNumber == 0) {
+				game.playerWins1++;
+				game.winnerNumber='0';
+			}
+			else if (winnerNumber == 1) {
+				game.playerWins2++;
+				game.winnerNumber='1';
+			}
+			else {
+				game.winnerNumber='-1';
+			}
 
+		}
+		Game.findByIdAndUpdate(game.id, game, function (err, previousValue) {
+		    if (err) {
+		    	callback({ status: 500, message: err } );
+		    }
+		    else {
+		    	callback(null, game);
+		    }
+		  });
+	});
+};
 
 var Game = mongoose.model('Game', gameSchema);
 
